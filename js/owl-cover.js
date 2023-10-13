eb032fceee5bef6d8f8a89107e0a74745f483512
Nicolas0729 <107268101+Nicolas0729@users.noreply.github.com>
2023-10-12 22:31:55 -0500

    cambios

---
+++ js/owl-cover.js
@@ -0,0 +1,27 @@
+$('.owl-cover').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin: 0,
+    autoplayTimeout:2000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        900:{
+            items:1
+        },
+        1000:{
+            items:1
+        },
+        1300:{
+            items:1
+      }
+    }
+})
